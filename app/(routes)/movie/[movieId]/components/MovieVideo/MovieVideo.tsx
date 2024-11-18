89fcaa44e05715cdb5416595174490fba4832564
JhonFredG <65980955+JhonFredG@users.noreply.github.com>
2024-11-18 12:47:10 -0500

    Update MovieVideo.tsx
    volviendo el mute a true

--- app/(routes)/movie/[movieId]/components/MovieVideo/MovieVideo.tsx
+++ app/(routes)/movie/[movieId]/components/MovieVideo/MovieVideo.tsx
@@ -16,7 +16,7 @@ export function MovieVideo(props: MovieVideoProps) {
       width="100%"
       height="100%"
       playing={true}
-      muted={false}
+      muted={true}
       controls={false}
     />
   );
